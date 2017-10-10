9ad25187bc67b1cc02fd5dccb91ebc9df7a33602
[Alp Deveci] <iceveda@gmail.com>
2017-10-10 13:45:40 -0500

    Adding restaurants functionality

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,9 +1,12 @@
 class Restaurant
 
     @@filepath = nil
+
     def self.filepath=(path=nil)
         @@filepath = File.join(APP_ROOT, path)
     end
+
+    attr_accessor :name, :cuisine, :price
 
     def self.file_exists?
         #class should know if the restaurant file exists
@@ -31,5 +34,13 @@ class Restaurant
     def self.saved_restaurants
         # read the restaurant file
         # return instances of restaurant
+    end
+
+    def save
+        return false unless Restaurant.file_usable?
+        File.open(@@filepath, 'a') do |file|
+            file.puts "#{[@name, @cuisine, @price].join("\t")}\n"
+        end
+        return true
     end
 end
